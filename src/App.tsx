54b0aea94dc5b2bef7b2adcbaee2e331ac6ef038
sajad-22 <am.betdevel@gmail.com>
2024-02-01 15:52:34 +0700

    updates in app.tsx and install pg-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,52 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import { ImageMagnifier } from './components';
 
 function App() {
+    const [images, setImages] = useState<string[]>([]);
     const [image, setImage] = useState<string | null>(null);
-    const [magnified, setMagnified] = useState<boolean>(false);
+    const [value, setValue] = useState<string>('');
+
+    const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        
+        const isFound = images.find(item => item.trim() === value.trim())
+
+        if (!isFound) {
+            setImages(prev => [value.trim(), ...prev]);
+        }
+        setValue('');
+    }
 
     return (
         <>
             <ImageMagnifier
-                image={magnified ? image : null}
-                setImage={(image: string | null) => { setMagnified(false); setImage(image) }}
+                image={image}
+                setImage={setImage}
             />
 
             <main className='main_app'>
-
-                <div className="main_app__form">
-                    <input
-                        type='url'
-                        required
-                        placeholder='https://domain.com/image.png'
-                        value={image ?? ''}
-                        onChange={(e) => setImage((e.target as HTMLInputElement).value)}
-                    />
-                    <button
-                        onClick={() => setMagnified(true)}
-                        type='button'
-                    >
-                        Magnify
-                    </button>
+                <form className="main_app__form" onSubmit={submitHandler}>
+                    <div className="main_app__form__container">
+                        <input
+                            type='url'
+                            required
+                            placeholder='https://example.com/image.png'
+                            value={value}
+                            onChange={e => setValue((e.target as HTMLInputElement).value)}
+                        />
+                        <button type='submit'>
+                            Add
+                        </button>
+                    </div>
+                </form>
+                <div className={'main_app__images'}>
+                    {images.map((url, i) => (
+                        <img key={`image_${url}_${i}`}
+                            onClick={() => setImage(url)}
+                            src={url}
+                            alt=''
+                        />
+                    ))}
                 </div>
             </main >
         </>
--- src/components/image-magnifier/index.tsx
+++ src/components/image-magnifier/index.tsx
@@ -1,16 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Icon } from "components";
 import cls from "./styles.module.scss";
 
 type Props = {
     image: string | null;
-    setImage: (image:string | null) => void;
+    setImage: (image: string | null) => void;
 }
+
 
 export function ImageMagnifier({ image, setImage }: Props) {
     const [background, setBackground] = useState<"blur" | "dark">("blur");
     const [imageWidth, setImageWidth] = useState<number>(100);
     const [imageRotation, setImageRotation] = useState<number>(0);
+
+
+    useEffect(() => {
+        const body = document.body;
+        image && (body.style.overflowY = 'hidden');
+
+        document.onkeydown = function (e) {
+
+            if (e.key === '-' || e.key === 'ArrowDown') {
+                setImageWidth(imageWidth > 10 ? imageWidth - 5 : imageWidth);
+                if (image) e.preventDefault();
+            }
+            else if (e.key === '+' || e.key === 'ArrowUp') {
+                setImageWidth(imageWidth < 150 ? imageWidth + 5 : imageWidth)
+                if (image) e.preventDefault();
+            }
+            else if (e.key === 'Escape') {
+                setImage(null);
+            }
+            else if (e.key === 'Backspace') {
+                setImageWidth(90)
+            }
+        }
+        document.onwheel = function (e) {
+            e.preventDefault();
+            const zoomOut = imageWidth > 10 ? imageWidth - 5 : imageWidth
+            const zoomIn = imageWidth < 150 ? imageWidth + 5 : imageWidth
+            setImageWidth(e.deltaY < 0 ? zoomIn : zoomOut);
+        }
+        return ()=> {
+            body.style.overflowY = 'auto';
+        }
+
+    }, [image, setImage, imageWidth, setImageWidth]);
 
     return (
         image ?
@@ -20,7 +55,7 @@ export function ImageMagnifier({ image, setImage }: Props) {
                     src={image}
                     className={cls.image}
                     width={imageWidth + "%"}
-                    style={{transform: `rotate(${imageRotation}deg)`}}
+                    style={{ transform: `rotate(${imageRotation}deg)` }}
                     onClick={() => { setImage(null); document.body.style.overflowY = 'auto'; }}
                 />
                 <div className={cls.controllers}>
@@ -35,21 +70,24 @@ export function ImageMagnifier({ image, setImage }: Props) {
                         </button>
                         <button
                             type="button"
-                            onClick={() => setImageRotation(imageRotation + 90)} title={("Rotate the image")}
+                            onClick={() => setImageRotation(imageRotation + 90)} 
+                            title={("Rotate the image")}
                             className={cls.button}
                         >
                             <Icon name="rotate-right-solid" color="white" />
                         </button>
                         <button
                             type="button"
-                            onClick={() => setImageWidth(imageWidth > 10 ? imageWidth - 5 : imageWidth)} title={("Zoom out")}
+                            onClick={() => setImageWidth(imageWidth > 10 ? imageWidth - 5 : imageWidth)} 
+                            title={("Zoom out")}
                             className={cls.button}
                         >
                             <Icon color="white" name="magnifying-glass-minus-solid" />
                         </button>
                         <button
                             type="button"
-                            onClick={() => setImageWidth(imageWidth < 150 ? imageWidth + 5 : imageWidth)} title={("Zoom in")}
+                            onClick={() => setImageWidth(imageWidth < 150 ? imageWidth + 5 : imageWidth)} 
+                            title={("Zoom in")}
                             className={cls.button}
                         >
                             <Icon color="white" name="magnifying-glass-plus-solid" />
